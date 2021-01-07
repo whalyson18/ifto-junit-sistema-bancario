8336a45b8333b2e6ce1af3eb816d1c0d64c22f20
Whalyson Ramos <warius18@gmail.com>
2021-01-07 14:54:59 -0300

    Alteração dia 16/12

--- src/main/java/com/sistemabancario/model/Conta.java
+++ src/main/java/com/sistemabancario/model/Conta.java
@@ -1,5 +1,6 @@
 package com.sistemabancario.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,24 +21,22 @@ public class Conta implements Cadastro {
      * Número que identifica unicamente uma conta em uma determinada agência,
      * devendo estar no formato 99999-9. Se o número não estiver no formato
      * indicado, o valor não pode ser armazenado e uma exceção deve ser lançada
-     * (R01). O número da agência tem um dígito verificador como no CPF, mas
-     * isto é outro requisito não definido aqui.
+     * (R01). O número da agência tem um dígito verificador como no CPF, mas isto é
+     * outro requisito não definido aqui.
      */
     private String numero;
 
     /**
-     * Contas devem ser instanciadas como "Conta Corrente" e não como
-     * "Poupança". (R02) O valor padrão para atributos boolean é false, assim
-     * não precisamos escrever código adicional para definir "poupanca" como
-     * false. Mas é preciso escrever o teste para verificar tal situação. Com
-     * isto, buscamos detectar se uma alteração no código fizer com que este
-     * requisito deixe de ser atendido.
+     * Contas devem ser instanciadas como "Conta Corrente" e não como "Poupança".
+     * (R02) O valor padrão para atributos boolean é false, assim não precisamos
+     * escrever código adicional para definir "poupanca" como false. Mas é preciso
+     * escrever o teste para verificar tal situação. Com isto, buscamos detectar se
+     * uma alteração no código fizer com que este requisito deixe de ser atendido.
      */
     private boolean poupanca;
 
     /**
-     * Indica se a conta é especial ou não. Caso seja, ela pode ter
-     * {@link #limite}.
+     * Indica se a conta é especial ou não. Caso seja, ela pode ter {@link #limite}.
      */
     private boolean especial;
 
@@ -48,19 +47,19 @@ public class Conta implements Cadastro {
 
     /**
      * Limite da conta: valor que o cliente pode utilizar além do {@link #saldo}
-     * disponível. Somente contas especiais podem ter limite, ou seja, o limite
-     * de contas "não especiais" não pode ser maior que zero (R03).
+     * disponível. Somente contas especiais podem ter limite, ou seja, o limite de
+     * contas "não especiais" não pode ser maior que zero (R03).
      */
     private double limite;
 
     /**
      * Histórico de movimentações da conta. Deve ser inicializado com uma lista
      * vazia. Sem isto, ao tentar utilizar a lista, dará o erro
-     * NullPointerException. Um teste deve verificar se, após instanciar uma
-     * conta usando qualquer um dos construtores, a lista de movimentações não é
-     * nula, chamando o método {@link #getMovimentacoes()}. (R04)
+     * NullPointerException. Um teste deve verificar se, após instanciar uma conta
+     * usando qualquer um dos construtores, a lista de movimentações não é nula,
+     * chamando o método {@link #getMovimentacoes()}. (R04)
      */
-    private List<Movimentacao> movimentacoes;
+    private List<Movimentacao> movimentacoes = new ArrayList<>();
 
     public Conta() {
         // TODO: Você precisa implementar este método
@@ -80,12 +79,11 @@ public class Conta implements Cadastro {
     }
 
     /**
-     * Adiciona uma nova movimentação à lista de {@link #movimentacoes}. (R05)
-     * Se a movimentação estiver confirmada, seu valor deve ser:
+     * Adiciona uma nova movimentação à lista de {@link #movimentacoes}. (R05) Se a
+     * movimentação estiver confirmada, seu valor deve ser:
      * <ul>
      * <li>somado ao saldo da conta caso o tipo da movimentação seja 'C';</li>
-     * <li>subtraída do saldo da conta caso o tipo da movimentação seja
-     * 'D'.</li>
+     * <li>subtraída do saldo da conta caso o tipo da movimentação seja 'D'.</li>
      * </ul>
      *
      * @param movimentacao {@link Movimentacao} a ser adicionada
@@ -101,24 +99,25 @@ public class Conta implements Cadastro {
      * @return
      */
     public double getSaldoTotal() {
-        /* TODO: Você precisa implementar este método. 
-        A linha abaixo deve ser substituída pelo seu código */
+        /*
+         * TODO: Você precisa implementar este método. A linha abaixo deve ser
+         * substituída pelo seu código
+         */
         return 0.0;
     }
 
     /**
-     * Registra uma nova movimentação para retirar um determinado valor da
-     * conta, caso o valor seja menor ou igual ao saldo total. Após realizar um
-     * saque, o saldo deve ser atualizado.
+     * Registra uma nova movimentação para retirar um determinado valor da conta,
+     * caso o valor seja menor ou igual ao saldo total. Após realizar um saque, o
+     * saldo deve ser atualizado.
      *
      * <p>
-     * Se for tentando realizar dois saques ao mesmo tempo com valor igual ao
-     * saldo total, somente o primeiro saque deve ser permitido. Isto evita que
-     * um cliente que possua dois cartões da mesma conta tente retirar mais
-     * dinheiro do que há disponível na conta. Ele poderia tentar fazer isso
-     * solicitando a ajuda de outra pessoa. Assim, as duas pessoas poderiam
-     * tentar ir em caixas diferentes ao mesmo tempo para tentar realizar um
-     * saque em duplicidade.
+     * Se for tentando realizar dois saques ao mesmo tempo com valor igual ao saldo
+     * total, somente o primeiro saque deve ser permitido. Isto evita que um cliente
+     * que possua dois cartões da mesma conta tente retirar mais dinheiro do que há
+     * disponível na conta. Ele poderia tentar fazer isso solicitando a ajuda de
+     * outra pessoa. Assim, as duas pessoas poderiam tentar ir em caixas diferentes
+     * ao mesmo tempo para tentar realizar um saque em duplicidade.
      * </p>
      *
      * @param valor valor a ser sacado (deve ser um valor positivo)
@@ -128,10 +127,10 @@ public class Conta implements Cadastro {
     }
 
     /**
-     * Adiciona uma nova movimentação de depósito em dinheiro tanto realizada
-     * por um funcionário quanto em um caixa eletrônico, que deve ser confirmada
-     * automaticamente. Considera-se que todos os caixas eletrônicos do banco
-     * têm capacidade de contar cédulas.
+     * Adiciona uma nova movimentação de depósito em dinheiro tanto realizada por um
+     * funcionário quanto em um caixa eletrônico, que deve ser confirmada
+     * automaticamente. Considera-se que todos os caixas eletrônicos do banco têm
+     * capacidade de contar cédulas.
      *
      * @param valor valor a ser depositado (deve ser um valor positivo)
      */
@@ -140,8 +139,8 @@ public class Conta implements Cadastro {
     }
 
     /**
-     * Adiciona uma nova movimentação de depósito em cheque (que deve ser
-     * confirmada posteriormente por um funcionário do banco).
+     * Adiciona uma nova movimentação de depósito em cheque (que deve ser confirmada
+     * posteriormente por um funcionário do banco).
      *
      * @param valor valor a ser depositado (deve ser um valor positivo)
      */
@@ -164,6 +163,9 @@ public class Conta implements Cadastro {
     }
 
     public void setNumero(String numero) {
+        if (!numero.matches("\\d{5}-\\d")) {
+            throw new IllegalArgumentException("Numero invalido. Deve estar no formato 99999-9");
+        }
         this.numero = numero;
     }
 
@@ -192,6 +194,9 @@ public class Conta implements Cadastro {
     }
 
     public void setLimite(double limite) {
+        if(!especial && limite > 0){
+            throw new IllegalStateException("Somente contas especiais podem ter limite");
+        }
         this.limite = limite;
     }
 }
--- src/test/java/com/sistemabancario/model/ContaTest.java
+++ src/test/java/com/sistemabancario/model/ContaTest.java
@@ -4,11 +4,64 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ContaTest {
+
+    @Test
+    void testSetNumeroValido() {
+        final Conta instance = new Conta();
+        final String esperado = "12345-6";
+        instance.setNumero(esperado);
+        final String obtido = instance.getNumero();
+        assertEquals(esperado, obtido);
+    }
+
+//    @Test
+//    void testSetNumeroInvalidoExcecao() {
+//        final Conta instance = new Conta();
+//        final String invalido = "123";
+//        assertThrows(IllegalArgumentException.class, () -> instance.setNumero(invalido));
+//    }
     
     @Test
-    void testAddMovimentacao() {
-        //TODO: Você precisa implementar este teste
+    void testSetNumeroinvalidoNaoArmazena(){
+        final Conta instance = new Conta();
+        final String invalido = "123";
+        assertThrows(IllegalArgumentException.class, () -> instance.setNumero(invalido));
+        final String obtido = instance.getNumero();
+        assertNotEquals(invalido, obtido);
     }
     
+    @Test
+    void testSetNumeroNulo(){
+        final Conta instance = new Conta();
+        assertThrows(NullPointerException.class, () -> instance.setNumero(null));
+    }
     
+    @Test
+    void testInstaciaPadraoPoupanca(){
+        final Conta instance = new Conta();
+        assertFalse(instance.isPoupanca());
+    }
+    
+    @Test
+    void testSetLimiteContaEspecial(){
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        final double esperado = 1000;
+        instance.setLimite(esperado);
+        final double obtido = instance.getLimite();
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testSetLimiteContaNaoEspecial(){
+        final Conta instance = new Conta();
+        final double limite = 1000;
+        assertThrows(IllegalStateException.class, () -> instance.setLimite(limite));
+    }
+    
+    @Test
+    void testHistoricoNotNull(){
+        final Conta instance = new Conta();
+        assertNotNull(instance.getMovimentacoes());
+    }
 }
